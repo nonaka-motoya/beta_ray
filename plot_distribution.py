80cb5d61a8e5902d8465eff6ea76264a87f8ef45
nonmot <nonmot0614@gmail.com>
2022-04-27 14:55:13 +0900

    make directory if does not exist

--- plot_distribution.py
+++ plot_distribution.py
@@ -1,11 +1,11 @@
 from betaDecay import betaDecay
 
-dirName = '20220421'
+dirName = 'data/20220421'
 
 bd = betaDecay(dirName=dirName)
 
-bd.plotRawData(dirName + '/wfm_2.txt')
+# bd.plotRawData(dirName + '/wfm_2.txt')
 bd.calcAreaDistribution()
 bd.plotDistribution(isLog=False,binNum=70)
-bd.calibration(Qarea=17500, Qenergy=2.28)
-bd.plotEnergyDistribution(isLog=False, binNum=70, binMin=0, binMax=3)
+# bd.calibration(Qarea=17500, Qenergy=2.28)
+# bd.plotEnergyDistribution(isLog=False, binNum=70, binMin=0, binMax=3)
